8de142f13e3defc09fe02b48b3bdb52d1174b66a
MichaelNguyenTT <michaelnguyenn94@gmail.com>
2023-11-13 15:12:25 -0800

    Added mountain select to display
    User selects a mountain. Clicks the button and displays information about the selected mountain.

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,46 +1,89 @@
-//  submitButton.addEventListener('click', async () => {
+'use strict'
 
-//  });
+window.onload = () => {
 
-function mountainTemplate(mountain) {
-  //let mySunrise = getSunsetForMountain(`${mountain.coords.lat},${mountain.coords.lng}`);
-
-  return `
-    <div class="mountain">
-    <img class="mountain-photo" src="images/${mountain.img}">
-    <h2 class="mountain-name">${mountain.name} <span class="species">(${mountain.elevation} feet)</span></h2>
-    <h4 class="mountain-desc">${mountain.desc} </h4>
-    <p><strong>Effort:</strong> ${mountain.effort}</p>
-    <strong>Coordinates:</strong> lat: ${mountain.coords.lat} lng: ${mountain.coords.lng}
-    <strong>Sunrise:</strong> ${mySunrise}
-    </div>
-    `;
-}
-
-document.getElementById("mountains").innerHTML = `
-  <h1 class="app-title"> ${mountainsArray.length} Mountains to climb</h1>
-  ${mountainsArray.map(mountainTemplate).join("")}
-  <p class="footer">These ${
-    mountainsArray.length
-  } mountants were added recently. Check back soon for updates.</p>
-`;
-
-async function getSunsetForMountain(lat, lng) {
-  let response = await fetch(
-    `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`
-  );
-  let data = await response.json();
-  return data;
-}
-
-function loadData() {
-  var down = document.getElementById("mountain");
-  for (let i = 0; i < mountainsArray.length; i++) {
-    var optn = mountainsArray[i];
-    var el = document.createElement("option");
-    el.textContent = optn;
-    el.value = optn;
-    down.appendChild(el);
+  const displayBtn = document.getElementById('displayMountainsBtn');
+  displayBtn.onclick = () => {
+    displayMountainInfo();
   }
-  down.innerHTML = "Elements Added";
+
+  const onMountainSelect = document.getElementById('mountain');
+  onMountainSelect.onchange = () => {
+    document.getElementById('mountains').innerHTML = ''
+    
+  }
+  loadMountainList();
 }
+
+function loadMountainList() {
+  const mountainSelect = document.getElementById('mountain');
+
+  mountainsArray.forEach((item) => {
+    let mountainOptions = new Option(item.name, item.name);
+    mountainSelect.appendChild(mountainOptions);
+  })
+};
+
+function getMountainInfo() {
+
+  const mountainSelect = document.getElementById('mountain').value;
+  const findMountain = mountainsArray.find((item) => item.name.includes(mountainSelect));
+
+  return findMountain;
+  
+};
+
+
+function displayMountainInfo() {
+
+  const getMountain = getMountainInfo();
+
+  document.getElementById('mountains').innerHTML = `
+  <div class="card" style="width: 18rem;">
+  <img src="/images/${getMountain.img}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${getMountain.name}</h5>
+    <p class="card-text">${getMountain.desc}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Elevation: ${getMountain.elevation}</li>
+    <li class="list-group-item">Effort: ${getMountain.effort}</li>
+    <li class="list-group-item">Coordinates:\n 
+    Latitude: ${getMountain.coords.lat} Longitude: ${getMountain.coords.lng}</li>
+  </ul>
+</div>`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function getSunsetForMountain(lat, lng) {
+//   let response = await fetch(
+//     `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`
+//   );
+//   let data = await response.json();
+//   return data;
+// }
+
+// function loadData() {
+//   var down = document.getElementById("mountain");
+//   for (let i = 0; i < mountainsArray.length; i++) {
+//     var optn = mountainsArray[i];
+//     var el = document.createElement("option");
+//     el.textContent = optn;
+//     el.value = optn;
+//     down.appendChild(el);
+//   }
+//   down.innerHTML = "Elements Added";
+// }
