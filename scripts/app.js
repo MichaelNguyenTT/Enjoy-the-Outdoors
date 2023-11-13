a78a8c6740c7dc1cef89af52988598fe205027ea
MichaelNguyenTT <michaelnguyenn94@gmail.com>
2023-11-13 13:58:31 -0800

    Search by Park Type completed
    Still work in progress. Will get back to updating it.

--- scripts/app.js
+++ scripts/app.js
@@ -12,10 +12,24 @@ window.onload = () => {
         clearParksList.innerHTML = '';
         updateParksOnLocationSelect();
     }
-    
     const locationBtn = document.getElementById('locationBtn');
     locationBtn.onclick = () => {
         searchLocation();
+    }
+
+
+    const parktypeBtn = document.getElementById('parktypeBtn');
+    parktypeBtn.onclick = () => {
+        searchByParkType();
+    }
+    const parktypeList = document.getElementById('parktype')
+    parktypeList.onchange = () => {
+        const clearDisplay = document.getElementById('myParks');
+        clearDisplay.innerHTML = '';
+
+        const clearParksList = document.getElementById('nationalParks');
+        clearParksList.innerHTML = '';
+        updateParksOnTypeSelect();
     }
     
     // Load the dropdowns
@@ -93,6 +107,45 @@ function updateParksOnLocationSelect() {
 //TODO function => get user park type value to loop thru all nationParksArray.locationName
 //TODO using case strings and have it display the parks
 
+function getParkNamesByType() {
+    const parktypeValue = document.getElementById('parktype').value;
+    const lowerParkType = parktypeValue.toLowerCase();
+ 
+    const newParkNames = nationalParksArray.filter((element) => element.LocationName.toLowerCase().includes(lowerParkType)); 
+    return newParkNames;
+};
+
+
+function updateParksOnTypeSelect() {
+    const statesValue = document.getElementById('states').value;
+    const nationalParksList = document.getElementById('nationalParks');
+    const parktypeNames = getParkNamesByType();
+
+    parktypeNames.forEach((element) => {
+        if (statesValue === element.State) {
+            let parkNameOptions = new Option(element.LocationName, element.State)
+            nationalParksList.appendChild(parkNameOptions)
+        } else {
+            console.log('not found');
+        }
+    })
+}
+
+function searchByParkType() {
+    const statesValue = document.getElementById('states').value;
+    const filteredParks = getParkNamesByType();
+
+    filteredParks.forEach((item) => {
+        if (statesValue === item.State) {
+            //* Displays when search By Location button is clicked:
+            let message = `${filteredParks.length} National Park to visit</h1><br><br>`;
+          
+            message += filteredParks.map(displayParks).join("");
+          
+            document.getElementById("myParks").innerHTML = message;
+        } 
+    })
+}
 
 function searchLocation() {
     const filteredParks = parksOnStateSelect();
