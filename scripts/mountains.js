a56e94cac3fe3f6571bff8adf8d29b03f960bcb7
MichaelNguyenTT <michaelnguyenn94@gmail.com>
2023-11-16 18:05:47 -0800

    Updated styling

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -3,6 +3,7 @@
 window.onload = () => {
   displayAllMountains();
   loadMountainList();
+  displayMountainInfo();
   
   const displayBtn = document.getElementById('displayMountainsBtn');
   displayBtn.onclick = () => {
@@ -17,9 +18,6 @@ window.onload = () => {
 
 function loadMountainList() {
   const mountainSelect = document.getElementById('mountain');
-
-  const preSelect = document.getElementById('mountain');
-  preSelect.appendChild(new Option('Choose A Mountain..', null));
 
   mountainsArray.forEach((item) => {
     let mountainOptions = new Option(item.name, item.name);
@@ -62,12 +60,7 @@ function displayAllMountains() {
 
 
 function displayMountainInfo() {
-  const mountainValue = document.getElementById('mountain').value;
   const getMountain = getMountainInfo();
-
-  if (mountainValue === null) {
-      return;
-  } else {
     document.getElementById('mountains').innerHTML =
     `
     <div class="modal fade" id="mountainModal" tabindex="-1" aria-labelledby="mountainModalLabel" aria-hidden="true">
@@ -75,15 +68,15 @@ function displayMountainInfo() {
       <div class="modal-content">
       <img src="/images/${getMountain.img}" class="card-img-top" alt="...">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="mountainModalLabel">${getMountain.name}</h1>
+          <h1 class="modal-title" id="mountainModalLabel">${getMountain.name}</h1>
         </div>
         <div class="modal-body">
           <p>${getMountain.desc}</p>
           <ul class="list-group list-group-flush">
             <li class="list-group-item"><strong>Elevation:</strong> ${getMountain.elevation}</li>
             <li class="list-group-item"><strong>Effort:</strong> <em>${getMountain.effort}</em></li>
-            <li class="list-group-item">
-            Latitude: ${getMountain.coords.lat} Longitude: ${getMountain.coords.lng}</li>
+            <li class="list-group-item"><strong>Latitude:</strong> ${getMountain.coords.lat}</li>
+            <li class="list-group-item"><strong>Longitude:</strong> ${getMountain.coords.lng}</li>
           </ul>
         </div>
       </div>
@@ -91,4 +84,3 @@ function displayMountainInfo() {
   </div>
     `
   }
-};
